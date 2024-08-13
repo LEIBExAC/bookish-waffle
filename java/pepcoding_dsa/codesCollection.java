b4ec9ad665d642f96dc6017404e6decf5ef51df8
Yuno <chaturvediaditya27@gmail.com>
2024-08-13 14:14:31 +0530

    3

---
+++ java/pepcoding_dsa/codesCollection.java
@@ -0,0 +1,23 @@
+package pepcoding_dsa;
+
+import java.util.*;
+
+class codesCollection {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        /*
+        * Using Scanner class to take int and String input from user will give some problem
+        * int n = sc.nextInt();
+        * String str = sc.nextLine();
+        * To address this we use Integer.parseInt(sc.nextLine()) to take int input
+        */
+        int n = Integer.parseInt(sc.nextLine());
+        String str = sc.nextLine();
+        sc.close();
+        System.out.println("Hey " + str + ". You entered " + n);
+        for (int i = 1; i >= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
--- java/pepcoding_dsa/prime.java
+++ java/pepcoding_dsa/prime.java
@@ -1,31 +1,34 @@
 package pepcoding_dsa;
 
-class prime{
-public static boolean isPrime(int n){
-if(n == 1){
-return false;
-}
-for(int i = 2; i < n/ 2; i++){
-if(n % i == 0){
-return false;}
-}
-return true;
-}
+import java.util.Scanner;
 
-public static void main(String[] args){
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter the number of primes you want to check: ");
-int t = sc.nextInt();
-for(int i = 0; i < t; i++){
-System.out.println("Enter the number you want to check: ");
-int n = sc.nextInt();
-if(isPrime(n)){
-System.out.println("The number " + n + " is prime
-");
-}else{
-System.out.println("The number " + n + " is not prime.");
+class prime {
+    public static boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i < n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of primes you want to check: ");
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            System.out.print("Enter the number you want to check: ");
+            int n = sc.nextInt();
+            if (isPrime(n)) {
+                System.out.println("The number " + n + " is prime.");
+            } else {
+                System.out.println("The number " + n + " is not prime.");
+            }
+            System.out.println("-----------------------------");
+        }
+        sc.close();
+    }
 }
-System.out.println("-----------------------------");
-}
-}
- }
