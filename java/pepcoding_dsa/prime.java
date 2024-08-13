78a140f047e1a5ae952ffc2a009aab921d1e23ec
Yuno <51988287+LEIBExAC@users.noreply.github.com>
2024-08-13 12:15:05 +0530

    3

--- java/pepcoding_dsa/prime.java
+++ java/pepcoding_dsa/prime.java
@@ -1,14 +1,12 @@
 package pepcoding_dsa;
 
-import java.util.Scanner;
-
 class prime{
 public static boolean isPrime(int n){
 if(n == 1){
 return false;
 }
-for(int i = 2; i <= Math.sqrt(n); i++){
-if(n % i != 0){
+for(int i = 2; i < n/ 2; i++){
+if(n % i == 0){
 return false;}
 }
 return true;
@@ -18,15 +16,16 @@ public static void main(String[] args){
 Scanner sc = new Scanner(System.in);
 System.out.println("Enter the number of primes you want to check: ");
 int t = sc.nextInt();
-for(int i = 0; i < t; t++){
+for(int i = 0; i < t; i++){
 System.out.println("Enter the number you want to check: ");
 int n = sc.nextInt();
 if(isPrime(n)){
-System.out.println("The number" + n + " is prime
+System.out.println("The number " + n + " is prime
 ");
 }else{
-System.out.println("The number" + n + " is not prime.");
+System.out.println("The number " + n + " is not prime.");
 }
+System.out.println("-----------------------------");
 }
 }
  }
