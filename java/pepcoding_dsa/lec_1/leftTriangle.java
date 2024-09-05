398b6e12a1d43bbd2b61136b88eb028efd86ea27
Yuno <chaturvediaditya27@gmail.com>
2024-09-05 22:18:18 +0530

    6

--- java/pepcoding_dsa/lec_1/leftTriangle.java
+++ java/pepcoding_dsa/lec_1/leftTriangle.java
@@ -1,10 +1,17 @@
 package pepcoding_dsa.lec_1;
+
+import java.util.Scanner;
+
 public class leftTriangle {
-    static void leftPattern(int n){
-        for(int i = 0; i < n; i++){
-           System.out.print("* ");
+    static void leftPattern(int n) {
+        for (int j = 1; j <= n; j++) {
+            for (int i = 1; i <= j; i++) {
+                System.out.print("* ");
+            }
+            System.out.println();
         }
     }
+
     public static void main(String[] args) {
         System.out.println("1st Method Left Triangle");
         for (int i = 1; i <= 5; i++) {
@@ -16,5 +23,12 @@ public class leftTriangle {
         System.err.println();
         System.out.println("2nd Method");
         System.out.println("*\n**\n***\n****\n*****");
+        System.err.println();
+        System.out.println("3rd Method");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        sc.close();
+        leftPattern(n);
     }
 }
